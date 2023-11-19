cf440ba319a3b690279378ee5b8da63b6580203a
guziool <guziool@gmx.com>
2023-11-19 01:14:50 +0100

    IF ELSE

--- 1stApp/1stApp/Program.cs
+++ 1stApp/1stApp/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, GoToIt Community!");
+﻿
+
+string myName = "Ewa";
+bool isWoman = true;
+int myAge = 33;
+char nameRecog = myName.Last();
+
+//Sprawdzenie płci na podstawie imienia
+if (nameRecog == 'a')
+{
+    isWoman = true;
+    Console.WriteLine("Imię konćzy się na a więc to kobieta");
+}
+else
+{
+    isWoman = false;
+    Console.WriteLine("Imię nie kończy się na a więc to mężczyzna");
+}
+
+if (isWoman == true)
+{
+    if (myAge < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30");
+    }
+    else if (myAge == 33 && myName == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (myAge < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
