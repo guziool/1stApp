73580c6bac1870d2a6d9fbdac11c78269d26be01
guziool <guziool@gmx.com>
2023-11-27 20:23:49 +0100

    TypeConversion

--- 1stApp/1stApp/Employee.cs
+++ 1stApp/1stApp/Employee.cs
@@ -1,25 +1,59 @@
-﻿namespace _1stApp
+﻿using System.Runtime.CompilerServices;
+
+namespace _1stApp
 {
     public class Employee
     {
         private List<float> grades = new List<float>();
-
         public Employee(string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
         }
-
         public string Name { get; private set; }
         public string Surname { get; private set; }
-
-
-
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
         }
-
+        public void AddGrade(long grade)
+        {
+            float gradeInLong = (float)grade;
+            this.AddGrade(gradeInLong);
+        }
+        public void AddGrade(double grade)
+        {
+            float gradeInDouble = (float)grade;
+            this.AddGrade(gradeInDouble);
+        }
+        public void AddGrade(int grade)
+        {
+            float gradeInInt = (float)grade;
+            this.AddGrade(gradeInInt);
+        }
+        public void AddGrade(decimal grade)
+        {
+            float gradeInDecimal = (float)grade;
+            this.AddGrade(gradeInDecimal);
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
--- 1stApp/1stApp/Program.cs
+++ 1stApp/1stApp/Program.cs
@@ -1,13 +1,13 @@
 ﻿using _1stApp;
 
 var employee1 = new Employee("Stefan", "Kowalski");
-employee1.AddGrade(1);
-employee1.AddGrade(2);
-employee1.AddGrade(-10);
-//employee1.AddGrade(3);
+employee1.AddGrade("Adam");
+employee1.AddGrade("101");
+employee1.AddGrade(10);
+employee1.AddGrade(100);
+employee1.AddGrade(0.004);
 var statistics = employee1.GetStatistics();
-var statistics2 = employee1.GetStatistics2();
-Console.WriteLine($"Tatal number of grades: {statistics.Counter}, secondary methed: {statistics2.Counter}" );
-Console.WriteLine($"Average: {statistics.Average:N2}, secondary method: {statistics2.Average:N2}");
-Console.WriteLine($"Max: {statistics.Max}, secondary method: {statistics2.Max}");
-Console.WriteLine($"Min: {statistics.Min}, secondary method: {statistics2.Min}");
+Console.WriteLine($"Tatal number of grades: {statistics.Counter}");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}");
